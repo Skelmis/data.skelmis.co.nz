3b84a51946bfc4f7cd7a429722bf7b9049270788
Skelmis <ethanmkh2@gmail.com>
2023-09-26 22:27:51 +1000

    feat: dockerize

--- piccolo_conf.py
+++ piccolo_conf.py
@@ -1,20 +1,22 @@
+import os
+
 from piccolo.engine.postgres import PostgresEngine
 from piccolo.engine.sqlite import SQLiteEngine
 
 from piccolo.conf.apps import AppRegistry
 
 
-# DB = PostgresEngine(
-#     config={
-#         "database": "piccolo_project",
-#         "user": "postgres",
-#         "password": "",
-#         "host": "localhost",
-#         "port": 5432,
-#     }
-# )
-DB = SQLiteEngine()
+if os.environ.get("PROD", None) is not None:
+    DB = PostgresEngine(
+        config={
+            "database": os.environ["POSTGRES_DB"],
+            "user": os.environ["POSTGRES_USER"],
+            "password": os.environ["POSTGRES_PASSWORD"],
+            "host": "localhost",
+            "port": 2201,
+        }
+    )
+else:
+    DB = SQLiteEngine()
 
-APP_REGISTRY = AppRegistry(
-    apps=["home.piccolo_app", "piccolo_admin.piccolo_app"]
-)
+APP_REGISTRY = AppRegistry(apps=["home.piccolo_app", "piccolo_admin.piccolo_app"])
