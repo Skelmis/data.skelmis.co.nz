27341d84e692ef0a71ef8a7cee20d690690ba1c6
skelmis <skelmis.craft@gmail.com>
2023-10-19 21:20:55 +1300

    feat: add contacts table

--- home/tables.py
+++ home/tables.py
@@ -1,2 +1,9 @@
 from piccolo.table import Table
-from piccolo.columns import Varchar, Boolean
+from piccolo.columns import Text
+
+
+class Contact(Table):
+    name: str = Text(primary_key=True, unique=True, index=True)
+    address: str = Text(required=False)
+    phone_number: str = Text(required=False)
+    notes: str = Text(required=False)
