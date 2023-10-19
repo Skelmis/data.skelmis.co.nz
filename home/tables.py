d59f30617f18ad22a777ef47e840cdcc98896e6e
skelmis <ethanmkh2@gmail.com>
2023-10-19 22:17:20 +1300

    chore: reorder fields

--- home/tables.py
+++ home/tables.py
@@ -4,6 +4,6 @@ from piccolo.columns import Text
 
 class Contact(Table):
     name: str = Text(unique=True, index=True)
-    address: str = Text(required=False)
     phone_number: str = Text(required=False)
+    address: str = Text(required=False)
     notes: str = Text(required=False)
