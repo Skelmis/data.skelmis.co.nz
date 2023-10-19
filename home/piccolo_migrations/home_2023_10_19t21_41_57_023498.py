26c81ce3f2e3bf5e8c333b4e22df3b7f811397a8
skelmis <ethanmkh2@gmail.com>
2023-10-19 21:42:26 +1300

    feat: add migrations for contact book

---
+++ home/piccolo_migrations/home_2023_10_19t21_41_57_023498.py
@@ -0,0 +1,104 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.column_types import Text
+from piccolo.columns.indexes import IndexMethod
+
+
+ID = "2023-10-19T21:41:57:023498"
+VERSION = "0.121.0"
+DESCRIPTION = ""
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="home", description=DESCRIPTION
+    )
+
+    manager.add_table(
+        class_name="Contact", tablename="contact", schema=None, columns=None
+    )
+
+    manager.add_column(
+        table_class_name="Contact",
+        tablename="contact",
+        column_name="name",
+        db_column_name="name",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": True,
+            "unique": True,
+            "index": True,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Contact",
+        tablename="contact",
+        column_name="address",
+        db_column_name="address",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Contact",
+        tablename="contact",
+        column_name="phone_number",
+        db_column_name="phone_number",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Contact",
+        tablename="contact",
+        column_name="notes",
+        db_column_name="notes",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    return manager
