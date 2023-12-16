e7c8224292a3af71ce2539d942bbf321f5901cf0
skelmis <ethanmkh2@gmail.com>
2023-12-17 02:41:44 +1300

    feat: add Jobs table

---
+++ home/piccolo_migrations/home_2023_12_17t02_34_33_079129.py
@@ -0,0 +1,106 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.column_types import Date
+from piccolo.columns.column_types import Text
+from piccolo.columns.defaults.date import DateNow
+from piccolo.columns.indexes import IndexMethod
+
+
+ID = "2023-12-17T02:34:33:079129"
+VERSION = "0.121.0"
+DESCRIPTION = ""
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="home", description=DESCRIPTION
+    )
+
+    manager.add_table(
+        class_name="Jobs", tablename="jobs", schema=None, columns=None
+    )
+
+    manager.add_column(
+        table_class_name="Jobs",
+        tablename="jobs",
+        column_name="start_date",
+        db_column_name="start_date",
+        column_class_name="Date",
+        column_class=Date,
+        params={
+            "default": DateNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Jobs",
+        tablename="jobs",
+        column_name="end_date",
+        db_column_name="end_date",
+        column_class_name="Date",
+        column_class=Date,
+        params={
+            "default": DateNow(),
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Jobs",
+        tablename="jobs",
+        column_name="job_title",
+        db_column_name="job_title",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Jobs",
+        tablename="jobs",
+        column_name="company",
+        db_column_name="company",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    return manager
--- home/tables.py
+++ home/tables.py
@@ -1,5 +1,7 @@
+import datetime
+
 from piccolo.table import Table
-from piccolo.columns import Text
+from piccolo.columns import Text, Date
 
 
 class Contact(Table):
@@ -8,3 +10,10 @@ class Contact(Table):
     email_address: str = Text(required=False)
     home_address: str = Text(required=False)
     notes: str = Text(required=False)
+
+
+class Jobs(Table):
+    start_date: datetime.datetime = Date(required=True)
+    end_date: datetime.datetime = Date(required=False, null=True)
+    job_title: str = Text(required=True)
+    company: str = Text(required=True)
